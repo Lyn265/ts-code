af626dc82d012303c5e55056d926459844d45497
Lyn265 <chenglliu2018@gmail.com>
2022-03-02 09:45:36 +0900

    ts爬虫工具

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,62 +1,33 @@
 //ts -> d.ts -> js
-import fs from 'fs'
 import path from 'path'
 import superagent from 'superagent'
-import * as cheerio from 'cheerio'
-interface CourseInfo {
-  title: string
-  count: number
-}
-interface CourseResult {
-  time: number
-  data: CourseInfo[]
-}
-interface Content {
-  [propName: number]: CourseInfo[]
+import DellAnalyzer from './dellAnalyzer'
+import fs from 'fs'
+
+export interface Analyzer {
+  analyze: (html: string, filePath: string) => string
 }
 class Crowller {
-  private secretKey = 'x3b174jsx'
   private filePath = path.resolve(__dirname, '../data/course.json')
-  private url = `http://www.dell-lee.com/typescript/demo.html?secrect=${this.secretKey}`
-  getCourseInfo(html: string) {
-    const courseInfo: CourseInfo[] = []
-    const $ = cheerio.load(html)
-    const courseItems = $('.course-item')
-    courseItems.map((index, element) => {
-      const descs = $(element).find('.course-desc')
-      const title = descs.eq(0).text()
-      const count = parseInt(descs.eq(1).text().split('：')[1], 10)
-      courseInfo.push({ title, count })
-    })
-    return {
-      time: new Date().getTime(),
-      data: courseInfo,
-    }
-  }
-
-  async getRawHtml() {
+  private async getRawHtml() {
     const result = await superagent.get(this.url)
     return result.text
   }
-  //存到json文件里
-  generateJsonContent(courseInfo: CourseResult) {
-    const fileContent: Content = {}
-    if (fs.existsSync(this.filePath)) {
-      let fileContent = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'))
-    }
-    fileContent[courseInfo.time] = courseInfo.data
-    return fileContent
+  private writeFile(content: string) {
+    fs.writeFileSync(this.filePath, content)
   }
 
-  async initPriderProcess() {
+  private async initPriderProcess() {
     const html = await this.getRawHtml()
-    const courseInfo = this.getCourseInfo(html)
-    const fileContent = this.generateJsonContent(courseInfo)
-    //写文件
-    fs.writeFileSync(this.filePath, JSON.stringify(fileContent))
+    const fileContent = this.analyzer.analyze(html, this.filePath) //写文件
+    this.writeFile(fileContent)
   }
-  constructor() {
+  constructor(private analyzer: any, private url: string) {
     this.initPriderProcess()
   }
 }
-const crowller = new Crowller()
+
+const secretKey = 'x3b174jsx'
+const url = `http://www.dell-lee.com/typescript/demo.html?secrect=${secretKey}`
+const analyzer = DellAnalyzer.getInstance()
+new Crowller(analyzer, url)
---
+++ src/dellAnalyzer.ts
@@ -0,0 +1,57 @@
+import * as cheerio from 'cheerio'
+import fs from 'fs'
+import { Analyzer } from './crowller'
+interface CourseInfo {
+  title: string
+  count: number
+}
+interface CourseResult {
+  time: number
+  data: CourseInfo[]
+}
+interface Content {
+  [propName: number]: CourseInfo[]
+}
+export default class DellAnalyzer implements Analyzer {
+  //单例模式 不允许外部类里实例化该类（DellAnalyzer）
+  private static instance: DellAnalyzer
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new DellAnalyzer()
+    }
+    return DellAnalyzer.instance
+  }
+
+  private getCourseInfo(html: string) {
+    const courseInfo: CourseInfo[] = []
+    const $ = cheerio.load(html)
+    const courseItems = $('.course-item')
+    courseItems.map((index, element) => {
+      const descs = $(element).find('.course-desc')
+      const title = descs.eq(0).text()
+      const count = parseInt(descs.eq(1).text().split('：')[1], 10)
+      courseInfo.push({ title, count })
+    })
+    return {
+      time: new Date().getTime(),
+      data: courseInfo,
+    }
+  }
+  //存到json文件里
+  private generateJsonContent(courseInfo: CourseResult, filePath: string) {
+    let fileContent: Content = {}
+    if (fs.existsSync(filePath)) {
+      //读取已有文件内容
+      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+    }
+    fileContent[courseInfo.time] = courseInfo.data
+    return fileContent
+  }
+  public analyze(html: string, filePath: string) {
+    const courseInfo = this.getCourseInfo(html)
+    const fileContent = this.generateJsonContent(courseInfo, filePath)
+    return JSON.stringify(fileContent)
+  }
+
+  private constructor() {}
+}
